ec65f23708a55b23a55054ed6eebc539be9b35e8
carlosdoria <carlosdoria953@gmail.com>
2022-04-11 23:14:31 -0300

    feat: remove components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,8 @@
 import type { NextPage } from 'next'
-import { ProductList } from '../components/ProductList'
 
 const Home: NextPage = () => {
-  const PRODUCTS = [
-    {
-      name: 'a',
-      description: 'description a'
-    },
-    {
-      name: 'b',
-      description: 'description b'
-    },
-    {
-      name: 'c',
-      description: 'description 3'
-    },
-  ]
-
   return (
-    <ProductList products={PRODUCTS} />
+    <main>Wow Games</main>
   )
 }
 
