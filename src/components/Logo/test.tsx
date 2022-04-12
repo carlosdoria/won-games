b7a50dfc99f2a6c491692839a323b8142c202d2c
carlosdoria <carlosdoria953@gmail.com>
2022-04-11 23:13:57 -0300

    feat: create the Logo component

---
+++ src/components/Logo/test.tsx
@@ -0,0 +1,15 @@
+import { screen, render } from '@testing-library/react';
+
+import { Logo } from './';
+
+describe('<Logo />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Logo />);
+
+    expect(
+      screen.getByRole('heading', { name: /Logo/i }),
+    ).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
