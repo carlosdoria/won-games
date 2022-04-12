ed58630101f8524e9cabfa53d0a322ace3879ac3
carlosdoria <carlosdoria953@gmail.com>
2022-04-11 23:56:00 -0300

    feat: implement svg and props

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -3,13 +3,7 @@ import { screen, render } from '@testing-library/react';
 import { Logo } from './';
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />);
+  it('should render a white label by default', () => {
 
-    expect(
-      screen.getByRole('heading', { name: /Logo/i }),
-    ).toBeInTheDocument();
-
-    expect(container.firstChild).toMatchSnapshot();
   });
 });
