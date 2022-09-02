7ee36184a832768ec832770a7a01bc60c1eb5984
carlosdoria <carlosdoria953@gmail.com>
2022-09-01 22:23:27 -0300

    feat: create a component banner

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,30 @@
+import { screen, render } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import { Banner } from './';
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<Banner {...props} />);
+
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /Defy death/i })).toBeInTheDocument();
+
+    expect(screen.getByRole('heading', { name: /Play the new CrashLands season/i })).toBeInTheDocument();
+
+    expect(screen.getByRole('link', { name: /Buy now/i })).toBeInTheDocument();
+
+    expect(screen.getByRole('link', { name: /Buy now/i })).toHaveAttribute('href', '/games/defy-death');
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
